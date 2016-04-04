a6189a5c5392364b791e95b91bbd35369a1a1da2
Martin Mroz <martinmroz@gmail.com>
2016-04-03 19:09:26 -0700

    Next refactor attempt for the parser. Will try something yet simpler.

--- src/assembler/parser.rs
+++ src/assembler/parser.rs
@@ -1,6 +1,7 @@
 
 use std::ascii::AsciiExt;
 use std::iter::Peekable;
+use std::collections::BTreeMap;
 
 /**
  program ::= statement_list
@@ -15,218 +16,225 @@ use std::iter::Peekable;
     | label
     | instruction
     | comment
- 
+
  directive ::= "." <identifier> (<string_literal> | <numeric_literal>)
 
  label ::= <identifier> ":"
+
+ instruction ::= <identifier> instruction_args_for(instruction)
  */
 
 use assembler::scanner::Token;
 
-// MARK: - Instruction Formats
-
-fn is_i0_instruction<'a>(instruction: &'a str) -> bool {
-    match instruction.to_ascii_lowercase().as_str() {
-        "nop" | "cls" | "ret" | "trap" | "trapret" => { true }
-        _ => { false }
-    }
-}
-
-fn is_iA_instruction<'a>(instruction: &'a str) -> bool {
-    match instruction.to_ascii_lowercase().as_str() {
-        "jp" | "call" => { true }
-        _ => { false }
-    }
-}
-
 // MARK: - Helper Macros
 
-macro_rules! expect {
-    ($scanner:expr, $expected:pat) => {{
-        let token = $scanner.next().unwrap();
-        match token {
-            $expected => {}
-            _ => {
-                println!("Invalid token {:?}", token);
-                return;
-            }
-        }
-    }};
-}
-
-macro_rules! accept {
-    ($scanner:expr, $expected:pat) => {{
-        let mut accepted = false;
+macro_rules! peek {
+    ($scanner:expr,$p:pat) => {{
         if let Some(token) = $scanner.peek() {
-            match token {
-                $expected => { accepted = true; }
-                _ => { }
+            match *token {
+                $p => { true  }
+                 _ => { false }
             }
+        } else {
+            false
         }
-        if accepted {
-            $scanner.next();
-        }
-        accepted
     }}
 }
 
-// MARK: - State Handlers
-
-fn syntax_error<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    /*
-     An invalid token not matching a terminal or a non-terminal was reached.
-     */
-    println!("Unexpected {:?} found.", scanner.next().unwrap());
-    println!("Terminating assembly.");
-    scanner.last();
-}
-
-// MARK: - Helper Methods
-
-fn accept_identifier<'a, I>(scanner: &mut Peekable<I>) -> Option<&'a str> where I: Iterator<Item=Token<'a>> {
-    let mut value = None;
-    if let Some(identifier_token) = scanner.peek() {
-        match identifier_token {
-            &Token::Identifier(string) => { value = Some(string); }
-            _ => {}
+macro_rules! accept {
+    ($scanner:expr,$p:pat) => {{
+        if peek!($scanner, $p) { 
+            (true, $scanner.next()) 
+        } else {
+            (false, None)
         }
-    }
-    if let Some(_) = value {
-        scanner.next();
-    }
-    value
+    }}
 }
 
-fn accept_numeric_literal<'a, I>(scanner: &mut Peekable<I>) -> Option<usize> where I: Iterator<Item=Token<'a>> {
-    let mut value = None;
-    if let Some(numeric_literal_token) = scanner.peek() {
-        match numeric_literal_token {
-            &Token::NumericLiteral(numeric) => { value = Some(numeric); }
-            _ => {}
-        }
-    }
-    if let Some(_) = value {
-        scanner.next();
-    }
-    value
+// MARK: - Type Definitions
+
+type ParseResult = Result<(), String>;
+
+// MARK: - Parser Object
+
+struct Parser<'a,I> where I: Iterator<Item=Token<'a>> {
+    /// Peekable iterator over input token stream.
+    scanner: Peekable<I>,
+    /// Stack of value-type tokens (Identifier, StringLiteral, NumericLiteral).
+    value_stack: Vec<Token<'a>>,
+    /// A map of labels and their corresponding addresses.
+    label_map: BTreeMap<&'a str, usize>,
+    /// Current address.
+    current_address: usize
 }
 
-fn accept_string_literal<'a, I>(scanner: &mut Peekable<I>) -> Option<&'a str> where I: Iterator<Item=Token<'a>> {
-    let mut value = None;
-    if let Some(string_literal_token) = scanner.peek() {
-        match string_literal_token {
-            &Token::StringLiteral(string) => { value = Some(string); }
-            _ => {}
-        }
-    }
-    if let Some(_) = value {
-        scanner.next();
-    }
-    value
-}
+impl<'a,I> Parser<'a,I> where I: Iterator<Item=Token<'a>> {
 
-// MARK: - Rules
-
-fn comment<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    expect!(scanner, Token::SingleLineComment(_));
-}
-
-fn label_or_instruction<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    let mut successful_evaluation = false;
-
-    /*
-     label ::= <identifier> ":"
-     instruction ::= 
-        [i0] (nop/cls/ret/trap/trapret)
-        [iA] (jp/call)
-     */
-
-    let identifier = accept_identifier(scanner).unwrap();
-
-    // Defines a new label at the current evaluation point.
-    if accept!(scanner, &Token::LabelMarker) {
-        println!("LABEL({})", identifier);
-        return;
-    }
-
-    // These instructions do not accept parameters.
-    if is_i0_instruction(identifier) {
-        println!("INSTRUCTION({})", identifier);
-        return;
-    }
-
-    // These instructions accept an address parameter.
-    if is_iA_instruction(identifier) {
-        if let Some(address) = accept_numeric_literal(scanner) {
-            println!("INSTRUCTION({} {})", identifier, address);
-            return;
+    fn new(scanner: I) -> Parser<'a, I> {
+        Parser {
+            scanner: scanner.peekable(),
+            value_stack: Vec::new(),
+            label_map: BTreeMap::new(),
+            current_address: 0
         }
     }
 
-    // The subsequent tokens did not match the rule.
-    if !successful_evaluation {
-        syntax_error(scanner);
-    }
-}
+    /// MARK: - Expecting values.
 
-fn directive<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    let mut successful_evaluation = false;
-
-    /*
-     directive ::= "." <identifier> (<string_literal> | <numeric_literal>)
-     */
-    expect!(scanner, Token::DirectiveMarker);
-
-    // Extract the directive ID if possible.
-    if let Some(identifier) = accept_identifier(scanner) {
-        if let Some(numeric_literal) = accept_numeric_literal(scanner) {
-            println!("Directive({}) Numeric({})", identifier, numeric_literal);
-            successful_evaluation = true;
-        }
-
-        if let Some(string_literal) = accept_string_literal(scanner) {
-            println!("Directive({}) String({})", identifier, string_literal);
-            successful_evaluation = true;
+    fn accept_identifier(&mut self) -> Option<&'a str> {
+        if let (true, Some(Token::Identifier(string))) = accept!(self.scanner, Token::Identifier(_)) {
+            Some(string)
+        } else {
+            None
         }
     }
 
-    // The subsequent tokens did not match a directive declaration.
-    if !successful_evaluation {
-        syntax_error(scanner);
+    fn accept_string(&mut self) -> Option<&'a str> {
+        if let (true, Some(Token::StringLiteral(string))) = accept!(self.scanner, Token::StringLiteral(_)) {
+            Some(string)
+        } else {
+            None
+        }
     }
-}
 
-fn statement<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    /*
-     statement ::= comment | directive | label | instruction
-     */
-    let mut next_rule: fn(&mut Peekable<I>) -> () = syntax_error;
+    fn accept_numeric(&mut self) -> Option<usize> {
+        if let (true, Some(Token::NumericLiteral(number))) = accept!(self.scanner, Token::NumericLiteral(_)) {
+            Some(number)
+        } else {
+            None
+        }
+    }
 
-    // Continue parsing the input file until it yields no more tokens.
-    if let Some(next_token) = scanner.peek() {
-        next_rule = match next_token {
-            &Token::SingleLineComment(_) => { comment }
-            &Token::DirectiveMarker => { directive }
-            &Token::Identifier(_) => { label_or_instruction }
-            _ => { syntax_error }
+    /// MARK: - Non-terminal Symbols.
+
+    pub fn parse(&mut self) -> ParseResult {
+        /*
+         statement_list ::= statement | statement_list | .
+         */
+        while let Some(_) = self.scanner.peek() {
+            try!(self.parse_statement());
+        }
+        Ok(())
+    }
+
+    fn parse_statement(&mut self) -> ParseResult {
+        /*
+         statement ::= comment | directive | label | instruction
+         */
+
+        // Consume the single-line comment token as it appears.
+        if let (true, _) = accept!(self.scanner, Token::SingleLineComment(_)) {
+            return Ok(());
+        }
+
+        // A directive begins with a directive marker token.
+        if peek!(self.scanner, Token::DirectiveMarker) {
+            return self.parse_directive();
+        }
+
+        if let Some(identifier) = self.accept_identifier() {
+            /*
+                   label ::= <identifier> ":"
+             instruction ::= <identifier> instruction_args_for(instruction)
+             */
+
+            // Both 'instruction' and 'label' begin with identifiers, and label is proceeded by a LabelIdentifier.
+            if let (true, _) = accept!(self.scanner, Token::LabelMarker) {
+                return self.interpret_label(identifier);
+            }
+
+            // This must, therefore, be an instruction.
+            return self.parse_instruction_args_with_identifier(identifier);
+        }
+
+        Err(format!("Unable to parse statement"))
+    }
+
+    fn parse_directive(&mut self) -> ParseResult {
+        /*
+         directive ::= <identifier> (<string_literal> | <numeric_literal>)
+         */
+        
+        // Expect the presence of  directive marker.
+        if let (false, None) = accept!(self.scanner, Token::DirectiveMarker) {
+            return Err(format!("Unexpected token {:?}. Expecting directive marker.", self.scanner.peek()));
+        }
+
+        // Expect the presence of an identifier.
+        let directive = if let Some(identifier) = self.accept_identifier() {
+            identifier
+        } else {
+            return Err(format!("Expected identifier after directive marker."));
         };
+
+        // Expect either a numeric or a string literal, and interpret accordingly.
+        if let Some(string) = self.accept_string() {
+            self.interpret_directive_s(directive, string)
+        } else if let Some(numeric) = self.accept_numeric() {
+            self.interpret_directive_n(directive, numeric)
+        } else {
+            Err(format!("Expected literal (string or numeric) for directive .{}", directive))
+        }
     }
 
-    next_rule(scanner);
+    fn parse_instruction_args_with_identifier(&mut self, instruction: &'a str) -> ParseResult {
+        /*
+         instruction ::= <identifier> instruction_args_for(instruction)
+         */
+        Ok(())
+    }
+
+    /// MARK: - Interpreters.
+
+    fn interpret_directive_s(&mut self, name: &'a str, string_arg: &'a str) -> ParseResult {
+        println!("Directive<Type=String>('{}', '{}')", name, string_arg);
+
+        // Emit a string literal.
+        if name.eq_ignore_ascii_case("db") {
+            return Ok(())
+        }
+
+        Err(format!("Unknown directive {} [Type=String] ('{}')", name, string_arg))
+    }
+
+    fn interpret_directive_n(&mut self, name: &'a str, numeric_arg: usize) -> ParseResult {
+        println!("Directive<Type=Numeric>('{}', ${:X})", name, numeric_arg);
+
+        // Set the current output address for the assembly.
+        if name.eq_ignore_ascii_case("org") {
+            self.current_address = numeric_arg;
+            return Ok(())
+        }
+
+        // Emit a single byte numeric literal.
+        if name.eq_ignore_ascii_case("db") {
+            if (numeric_arg & 0xFF) != 0 {
+                return Err(format!("Cannot emit byte with numeric value > 0xFF (was ${:X})", numeric_arg));
+            } else {
+                return Ok(());
+            }
+        }
+
+        Err(format!("Unknown directive {} [Type=Numeric] (${:X})", name, numeric_arg))
+    }
+
+    fn interpret_label(&mut self, name: &'a str) -> ParseResult {
+        // An attempt was made to re-define the label.
+        if let Some(&existing_address) = self.label_map.get(name) {
+            return Err(format!("Attempted re-definition of label '{}' (${:X}) to ${:X}", name, existing_address, self.current_address))
+        }
+
+        // Store the current address in the label map.
+        println!("DefineLabel('{}') = ${:X}", name, self.current_address);
+        self.label_map.insert(name, self.current_address);
+        Ok(())
+    }
+
 }
 
-fn statement_list<'a, I>(scanner: &mut Peekable<I>) where I: Iterator<Item=Token<'a>> {
-    /*
-     statement_list ::= statement_list statement | statement | .
-     */
-    while let Some(_) = scanner.peek() {
-        statement(scanner);
-    }
-}
+// MARK: - Parse Function
 
 pub fn parse<'a, I>(scanner: I) where I: Iterator<Item=Token<'a>> {
-    /*
-     program ::= statement_list
-     */
-    statement_list(&mut scanner.peekable());
+    let mut parser = Parser::new(scanner);
+    println!("{:?}", parser.parse());
 }
