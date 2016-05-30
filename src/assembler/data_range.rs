19e1d7f3fdd16285946b8e7f1ed9c1b2e658da55
Martin Mroz <martinmroz@gmail.com>
2016-05-30 14:36:27 -0700

    Add support for data range intersection checks.

--- src/assembler/data_range.rs
+++ src/assembler/data_range.rs
@@ -1,4 +1,5 @@
 
+use std::cmp;
 use std::ops::Range;
 
 use assembler::u12;
@@ -70,10 +71,53 @@ impl DataRange {
     }
   }
 
+  /**
+   @return YES if the supplied range intersects the receiver.
+   */
+  pub fn intersects(&self, range: &DataRange) -> bool {
+    let lhs_range =  self.address_range();
+    let rhs_range = range.address_range();
+
+    // Ranges intersect if the greater of the two starts is less than the smaller of the two ends (half-open).
+    cmp::max(lhs_range.start, rhs_range.start) < cmp::min(lhs_range.end, rhs_range.end)
+  }
+
 }
+
+// MARK: - Public Functions
+
+/**
+ @param ranges A list of data ranges.
+ @return A list of pairs of overlapping data ranges.
+ */
+pub fn find_overlapping_data_ranges<'a>(ranges: &'a [DataRange]) -> Vec<(&'a DataRange, &'a DataRange)> {
+  let mut overlapping_ranges = Vec::new();
+
+  /*
+   Compare each data range to each other data range. This is done by evaluating the mathematical
+   combination pairs of the data range (order does not matter, pairs must be unique). This has
+   complexity O(ranges.len() nCr 2).
+   */
+  for i in 0..ranges.len() {
+    for j in i+1..ranges.len() {
+      let lhs = &ranges[i];
+      let rhs = &ranges[j];
+
+      if lhs.intersects(rhs) {
+        overlapping_ranges.push((lhs, rhs));
+      }
+    }
+  }
+
+  overlapping_ranges
+}
+
+// MARK: - Tests
 
 #[cfg(test)]
 mod tests {
+
+  use std::iter::repeat;
 
   use super::*;
 
@@ -147,6 +191,67 @@ mod tests {
 
     // Validate the data was not appended.
     assert_eq!(max_range.data(), &vec![1]);
+  }
+
+  #[test]
+  fn test_intersects() {
+    // Case 1: Obviously disjoint (+++++===++), distance: 3.
+    let disjoint_1a = range_with_start_and_length(0, 5);
+    let disjoint_1b = range_with_start_and_length(8, 2);
+    assert_eq!(disjoint_1a.intersects(&disjoint_1b), false);
+    assert_eq!(disjoint_1b.intersects(&disjoint_1a), false);
+
+    // Case 2: Tightly disjoint (+++++===++), distance: 1.
+    let disjoint_2a = range_with_start_and_length(0, 5);
+    let disjoint_2b = range_with_start_and_length(6, 4);
+    assert_eq!(disjoint_2a.intersects(&disjoint_2b), false);
+    assert_eq!(disjoint_2b.intersects(&disjoint_2a), false);
+
+    // Case 3: Adjacent (++++++++++), distance: 0.
+    let adjacent_a = range_with_start_and_length(0, 5);
+    let adjacent_b = range_with_start_and_length(5, 5);
+    assert_eq!(adjacent_a.intersects(&adjacent_b), false);
+    assert_eq!(adjacent_b.intersects(&adjacent_a), false);
+
+    // Case 4: Tail Overlap (++++++v+++) distance: -1.
+    let tail_overlap_a = range_with_start_and_length(0, 7);
+    let tail_overlap_b = range_with_start_and_length(6, 4);
+    assert_eq!(tail_overlap_a.intersects(&tail_overlap_b), true);
+    assert_eq!(tail_overlap_b.intersects(&tail_overlap_a), true);
+
+    // Case 5: Wholly Contained Head (v+++++++++) distance: -1.
+    let wholly_contained_head_a = range_with_start_and_length(0, 10);
+    let wholly_contained_head_b = range_with_start_and_length(0, 1);
+    assert_eq!(wholly_contained_head_a.intersects(&wholly_contained_head_b), true);
+    assert_eq!(wholly_contained_head_b.intersects(&wholly_contained_head_a), true);
+
+    // Case 6: Wholly Contained Tail (+++++++++v) distance: -1.
+    let wholly_contained_tail_a = range_with_start_and_length(0, 10);
+    let wholly_contained_tail_b = range_with_start_and_length(9, 1);
+    assert_eq!(wholly_contained_tail_a.intersects(&wholly_contained_tail_b), true);
+    assert_eq!(wholly_contained_tail_b.intersects(&wholly_contained_tail_a), true);
+
+    // Case 7: Middle of Range (++++vv++++) distance: -2.
+    let middle_a = range_with_start_and_length(0, 10);
+    let middle_b = range_with_start_and_length(4, 2);
+    assert_eq!(middle_a.intersects(&middle_b), true);
+    assert_eq!(middle_b.intersects(&middle_a), true);
+
+    // Case 7: Zero Length Range (++++++++++) distance: 0.
+    let zero_a = range_with_start_and_length(0, 10);
+    let zero_b = range_with_start_and_length(4, 0);
+    assert_eq!(zero_a.intersects(&zero_b), false);
+    assert_eq!(zero_b.intersects(&zero_a), false);
+  }
+
+  // MARK: - Helper Methods
+
+  /// Construct a new zero-filled range at the start address provided with the length provided.
+  fn range_with_start_and_length(start: usize, length: usize) -> DataRange {
+    let mut range = DataRange::new(start.as_u12().unwrap());
+    let data: Vec<u8> = repeat(0u8).take(length).collect();
+    range.append(data.as_slice());
+    range
   }
 
 }
