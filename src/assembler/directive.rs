a01af80bb982fb131e26b1cf05cda1015ed4549b
Martin Mroz <martinmroz@gmail.com>
2016-05-29 18:54:13 -0700

    Break the Directive semantic analysis and emit code into its own module and Enum.

--- src/assembler/directive.rs
+++ src/assembler/directive.rs
@@ -1,22 +1,9 @@
 
+use assembler::assembler::Emittable;
 use assembler::parser::Literal;
 use assembler::u12::*;
 
-pub trait Emittable {
-
-  /**
-   @return The size of the emittable element in bytes.
-   */
-  fn size(&self) -> usize;
-
-  /**
-   Consuming conversion from an emittable element into an byte Vec.
-   @return A Vec of bytes to be written into the output stream.
-   */
-  fn into_slice(self) -> Vec<u8>;
-
-}
-
+#[derive(PartialEq, Eq, Debug)]
 pub enum Directive {
   /// Directive to set the emit origin to the new address.
   Org(U12),
@@ -24,7 +11,7 @@ pub enum Directive {
   Db(Vec<u8>)
 }
 
-pub mod names {
+mod names {
   /// The name of the 'origin' directive (.org)
   pub const ORG: &'static str = "org";
   /// The name of the 'define-bytes' directive (.db)
@@ -38,7 +25,7 @@ impl<'a> Directive {
    @param arguments The literal arguments specified for the directive.
    @return Directive in the event the arguments are valid for the directive, or a failure reason.
    */
-  fn from_identifier_and_parameters(identifier: &'a str, arguments: &Vec<Literal>) -> Result<Directive,String> {
+  pub fn from_identifier_and_parameters(identifier: &'a str, arguments: &Vec<Literal>) -> Result<Directive,String> {
     /*
      The 'org' directive is used to set the current output origin address. The directive requires
      a single numeric literal in the range $000-$FFF.
@@ -68,7 +55,7 @@ impl<'a> Directive {
         return Err(format!("Incorrect number of parameters (0) for directive .db, expecting 1 or more."));
       }
 
-      let mut bytes = Vec::<u8>::new();
+      let mut bytes = Vec::new();
 
       // Convert all valid parameters into their byte literal values.
       for argument in arguments {
@@ -113,6 +100,83 @@ impl Emittable for Directive {
     match self {
       Directive::Org(_)       => vec![],
       Directive::Db(data)     => data
+    }
+  }
+
+}
+
+// MARK: - Tests
+
+#[cfg(test)]
+mod tests {
+
+  use assembler::assembler::Emittable;
+  use assembler::parser::Literal;
+  use assembler::u12;
+  use assembler::u12::*;
+
+  use super::*;
+
+  #[test]
+  fn test_validate_directive_semantics_for_org() {
+    let mut params = vec![];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Err("Incorrect number of parameters (0) for directive .org, expecting 1.".to_string()));
+    params = vec![Literal::Numeric(1), Literal::Numeric(3)];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Err("Incorrect number of parameters (2) for directive .org, expecting 1.".to_string()));
+    params = vec![Literal::Numeric(0x1000)];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Err("Directive .org requires 1 numeric literal in the range $000-$FFF.".to_string()));
+    params = vec![Literal::String("TEST_STRING")];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Err("Directive .org requires 1 numeric literal in the range $000-$FFF.".to_string()));
+    params = vec![Literal::Numeric(0xFFF)];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Ok(Directive::Org(u12::MAX)));
+    params = vec![Literal::Numeric(0x000)];
+    assert_eq!(Directive::from_identifier_and_parameters("org", &params), Ok(Directive::Org(u12::MIN)));
+  }
+
+  #[test]
+  fn test_validate_directive_semantics_for_db() {
+    let mut params = vec![];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Err("Incorrect number of parameters (0) for directive .db, expecting 1 or more.".to_string()));
+    params = vec![Literal::Numeric(0x100)];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Err("All numeric parameters to .db must be 1-byte literals ($100 > $FF)".to_string()));
+    params = vec![Literal::String("TEST_STRING")];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Ok(Directive::Db("TEST_STRING".as_bytes().to_vec())));
+    params = vec![Literal::Numeric(0xFF)];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Ok(Directive::Db(vec![0xFF])));
+    params = vec![Literal::Numeric(0x00)];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Ok(Directive::Db(vec![0x00])));
+    params = vec![Literal::String("TEST_STRING"), Literal::Numeric(0x00)];
+    assert_eq!(Directive::from_identifier_and_parameters("db", &params), Ok(Directive::Db("TEST_STRING\0".as_bytes().to_vec())));
+  }
+
+  #[test]
+  fn test_validate_directive_semantics_for_invalid_directive() {
+    let mut params = vec![];
+    assert_eq!(Directive::from_identifier_and_parameters("dw", &params), Err("Unrecognized directive .dw".to_string()));
+    params = vec![Literal::Numeric(0x100)];
+    assert_eq!(Directive::from_identifier_and_parameters("dw", &params), Err("Unrecognized directive .dw".to_string()));
+  }
+
+  #[test]
+  fn test_size_of_directive_for_org() {
+    let org_directive = Directive::from_identifier_and_parameters("org", &vec![Literal::Numeric(0xFFF)]).ok().unwrap();
+    assert_eq!(org_directive.size(), 0);
+  }
+
+  #[test]
+  fn test_size_of_directive_for_db() {
+    let expected_sizes = [1, 11, 12];
+    let directives = [
+      Directive::from_identifier_and_parameters("db", &vec![Literal::Numeric(0xFF)]).ok().unwrap(),
+      Directive::from_identifier_and_parameters("db", &vec![Literal::String("TEST_STRING")]).ok().unwrap(),
+      Directive::from_identifier_and_parameters("db", &vec![Literal::String("TEST_STRING"), Literal::Numeric(0x00)]).ok().unwrap()
+    ];
+
+    // Validate the expected size is obtained for each directive.
+    for (i, directive) in directives.iter().enumerate() {
+      let size = directive.size();
+      let expected_size = expected_sizes[i];
+      assert_eq!(size, expected_size);
     }
   }
 
