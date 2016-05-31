1843fd97ad85649c733defb77c9299828ac2e9b4
Martin Mroz <martinmroz@gmail.com>
2016-05-30 17:39:47 -0700

    Assemble directly into Intel HEX I8HEX format.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 
+use std::io::prelude::*;
+use std::fs::File;
+
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
@@ -7,9 +10,7 @@ pub mod assembler;
 use assembler::scanner::Scanner;
 use assembler::parser::*;
 use assembler::assembler::*;
-
-use std::io::prelude::*;
-use std::fs::File;
+use assembler::i8hex_writer::*;
 
 fn main() {
 
@@ -24,21 +25,24 @@ fn main() {
   let mut buffer = String::new();
   
   // Read the contents into the buffer.
-  file.read_to_string(&mut buffer)
-      .expect(format!("Unable to read contents of file {}", path).as_str());
+  file
+    .read_to_string(&mut buffer)
+    .expect(format!("Unable to read contents of file {}", path).as_str());
 
   // Create a scanner to process the contents.
   let scanner = Scanner::new("test.S", buffer.as_str());
-  match parse(scanner) {
-    Ok(asl) => {
-      for node in asl.iter() {
-        println!("{:?}", *node);
-      }
-      println!("{:?}", assemble(asl));
-    }
-    Err(reason) => {
-      println!("Parse failed: {}", reason);
-    }
-  }
-  
+  let asl = match parse(scanner) {
+    Ok(asl) => asl,
+    Err(reason) => panic!(reason)
+  };
+
+  // Assemble the ASL into data ranges.
+  let data_ranges = match assemble(asl) {
+    Ok(data_ranges) => data_ranges,
+    Err(reason) => panic!(reason)
+  };
+
+  // Convert the resultant data to Intel HEX (I8HEX version).
+  let convertible_ranges = data_ranges.iter().map(|range| range).collect::<Vec<_>>();
+  println!("{}", i8hex_representation_of_data_ranges(convertible_ranges.as_slice()));
 }
